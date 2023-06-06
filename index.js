e1e870b2b52e0b7f2d9a8f32c878a96ac5e256d3
marino-hash <marinostura76@gmail.com>
2023-06-07 00:57:03 +0200

    Kraj

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const sliderContainer = document.querySelector('.slider-container');
-const sliderPrev = document.querySelector('.slider-prev');
-const sliderNext = document.querySelector('.slider-next');
-const sliderRowWidth = sliderContainer.clientWidth;
-let currpos = 0;
-
-sliderPrev.addEventListener('click', () => {
-  currpos -= sliderRowWidth;
-  sliderContainer.style.transform = `translateX(${currpos}px)`;
-  console.log(currpos);
-  if (currpos < 0) {
-    currpos = 0;
+$(document).ready(function() {
+  const sliderContainer = $('.slider-container');
+  const sliderRow = $('.inner-slider');
+  const sliderRowbot = $('.inner-slider-bottom');
+  const sliderPrev = $('.prev');
+  const sliderNext = $('.next');
+  const imgTop = $('.rownum2 img')
+  const images = $('.slider-row img');
+  console.log(images.eq(4).width())
+  let currentSlide = images.length - 1;
+  let currentSlideUp = imgTop.length - 1;
+  var slidewidth = 0;
+  var slidewidthUp = 0;
+  sliderPrev.on('click', () => {
+    if(currentSlide === images.length - 1 ) return;
+    currentSlide = (currentSlide === images.length - 1) ? images.length - 1 : currentSlide + 1;
+    const translateY = imgTop.eq(currentSlideUp).width();
+    const translateX = images.eq(currentSlide).width();
+    slidewidth = slidewidth - translateX;
+    slidewidthUp = slidewidthUp - translateY;
+    console.log(slidewidth);
+    sliderRow.css('transform', `translateX(${slidewidthUp-5}px)`);
+    sliderRowbot.css('transform', `translateX(${slidewidth-22}px)`);
+  });
+  
+  sliderNext.on('click', () => {
+    if(currentSlide === 0  ) return;
+  const translateX = images.eq(currentSlide).width();
+  const translateY = imgTop.eq(currentSlideUp).width();
+  console.log(currentSlide);
+    currentSlide = (currentSlide === 0) ? 0 : currentSlide - 1;
+    console.log(currentSlide);
+     slidewidth = slidewidth + translateX;
+     slidewidthUp = slidewidthUp + translateY;
+     console.log(slidewidth);
+    sliderRow.css('transform', `translateX(${slidewidthUp+15}px)`);
+    sliderRowbot.css('transform', `translateX(${slidewidth+25}px)`);
     
-    sliderContainer.style.transform = `translateX(${currpos}px)`;
-  }
-});
-sliderNext.addEventListener('click', () => {
-  currpos += sliderRowWidth;
-  sliderContainer.style.transform = `translateX(${currpos}px)`;
-  console.log(currpos);
-  if (currpos > 0) {
-    currpos = -sliderContainer.offsetWidth;
-    
-    sliderContainer.style.transform = `translateX(${currpos}px)`;
-  }
- 
+  });
 });
