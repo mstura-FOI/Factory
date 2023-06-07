de5077503c7531ffb6cb82d1933f95af49919976
marino-hash <marinostura76@gmail.com>
2023-06-07 02:58:58 +0200

    Gotovo

--- index.js
+++ index.js
@@ -8,32 +8,39 @@ $(document).ready(function() {
   const images = $('.slider-row img');
   let currentSlide = images.length - 1;
   let currentSlideUp = imgTop.length - 1;
+
   var slidewidth = 0;
   var slidewidthUp = 0;
+  var growth = 0;
+  var negGrowth =35;
   sliderPrev.on('click', () => {
     if(currentSlide === images.length - 1 ) return;
+    
     currentSlide = (currentSlide === images.length - 1) ? images.length - 1 : currentSlide + 1;
-    const translateY = imgTop.eq(currentSlideUp).width();
+    currentSlideUp = (currentSlideUp === imgTop.length - 1) ? imgTop.length - 1 : currentSlideUp + 1;
+    
+    const translateY = imgTop.eq(currentSlideUp+1).width();
     const translateX = images.eq(currentSlide).width();
     slidewidth = slidewidth - translateX;
     slidewidthUp = slidewidthUp - translateY;
-    console.log(slidewidth);
-    console.log(slidewidthUp);
-    sliderRow.css('transform', `translateX(${slidewidthUp-38}px)`);
-    sliderRowbot.css('transform', `translateX(${slidewidth+20}px)`);
+    growth = growth-5;
+    console.log(translateY);
+    sliderRow.css('transform', `translateX(${slidewidthUp+growth}px)`);
+    sliderRowbot.css('transform', `translateX(${slidewidth+growth}px)`);
   });
   
   sliderNext.on('click', () => {
     if(currentSlide === 0  ) return;
+    
   const translateX = images.eq(currentSlide).width();
   const translateY = imgTop.eq(currentSlideUp).width();
     currentSlide = (currentSlide === 0) ? 0 : currentSlide - 1;
+    currentSlideUp = (currentSlide === 0) ? 0 : currentSlideUp - 1;
      slidewidth = slidewidth + translateX;
      slidewidthUp = slidewidthUp + translateY;
-     console.log(slidewidth);
-     console.log(slidewidthUp);
-    sliderRow.css('transform', `translateX(${slidewidthUp-38}px)`);
-    sliderRowbot.css('transform', `translateX(${slidewidth+25}px)`);
+     growth = growth+5;
+    sliderRow.css('transform', `translateX(${slidewidthUp+growth}px)`);
+    sliderRowbot.css('transform', `translateX(${slidewidth+growth}px)`);
     
   });
 });
